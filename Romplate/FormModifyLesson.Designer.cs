6e42ad170fdd65db93684f0569df4a6daeb787cb
Kiwii Vanya <kiwiivanya128@gmail.com>
2023-08-22 23:50:00 +0300

    made a better interface with scaling

---
+++ Romplate/FormModifyLesson.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Romplate
+{
+	partial class FormModifyLesson
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormModifyLesson));
+			textBox1 = new TextBox();
+			label1 = new Label();
+			label2 = new Label();
+			textBox2 = new TextBox();
+			textBox3 = new TextBox();
+			labelLink = new Label();
+			tableLayoutPanel1 = new TableLayoutPanel();
+			buttonCancel = new Button();
+			buttonApply = new Button();
+			tableLayoutPanel2 = new TableLayoutPanel();
+			tableLayoutPanel1.SuspendLayout();
+			tableLayoutPanel2.SuspendLayout();
+			SuspendLayout();
+			// 
+			// textBox1
+			// 
+			textBox1.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+			textBox1.Location = new Point(3, 23);
+			textBox1.Name = "textBox1";
+			textBox1.Size = new Size(290, 23);
+			textBox1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new Point(3, 0);
+			label1.Name = "label1";
+			label1.Size = new Size(39, 15);
+			label1.TabIndex = 1;
+			label1.Text = "Name";
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Location = new Point(3, 48);
+			label2.Name = "label2";
+			label2.Size = new Size(66, 15);
+			label2.TabIndex = 3;
+			label2.Text = "Homework";
+			// 
+			// textBox2
+			// 
+			textBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			textBox2.Location = new Point(3, 71);
+			textBox2.Multiline = true;
+			textBox2.Name = "textBox2";
+			textBox2.Size = new Size(290, 77);
+			textBox2.TabIndex = 2;
+			// 
+			// textBox3
+			// 
+			textBox3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			textBox3.Location = new Point(3, 174);
+			textBox3.Multiline = true;
+			textBox3.Name = "textBox3";
+			textBox3.Size = new Size(290, 77);
+			textBox3.TabIndex = 4;
+			// 
+			// labelLink
+			// 
+			labelLink.AutoSize = true;
+			labelLink.Location = new Point(3, 151);
+			labelLink.Name = "labelLink";
+			labelLink.Size = new Size(82, 15);
+			labelLink.TabIndex = 5;
+			labelLink.Text = "Link to a meet";
+			// 
+			// tableLayoutPanel1
+			// 
+			tableLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			tableLayoutPanel1.ColumnCount = 2;
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+			tableLayoutPanel1.Controls.Add(buttonApply, 1, 0);
+			tableLayoutPanel1.Controls.Add(buttonCancel, 0, 0);
+			tableLayoutPanel1.Location = new Point(3, 257);
+			tableLayoutPanel1.Name = "tableLayoutPanel1";
+			tableLayoutPanel1.RowCount = 1;
+			tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+			tableLayoutPanel1.Size = new Size(290, 42);
+			tableLayoutPanel1.TabIndex = 6;
+			// 
+			// buttonCancel
+			// 
+			buttonCancel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			buttonCancel.Location = new Point(3, 3);
+			buttonCancel.Name = "buttonCancel";
+			buttonCancel.Size = new Size(139, 36);
+			buttonCancel.TabIndex = 0;
+			buttonCancel.Text = "Cancel";
+			buttonCancel.UseVisualStyleBackColor = true;
+			// 
+			// buttonApply
+			// 
+			buttonApply.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			buttonApply.Location = new Point(148, 3);
+			buttonApply.Name = "buttonApply";
+			buttonApply.Size = new Size(139, 36);
+			buttonApply.TabIndex = 1;
+			buttonApply.Text = "Apply";
+			buttonApply.UseVisualStyleBackColor = true;
+			// 
+			// tableLayoutPanel2
+			// 
+			tableLayoutPanel2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			tableLayoutPanel2.ColumnCount = 1;
+			tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+			tableLayoutPanel2.Controls.Add(label1, 0, 0);
+			tableLayoutPanel2.Controls.Add(tableLayoutPanel1, 0, 6);
+			tableLayoutPanel2.Controls.Add(textBox1, 0, 1);
+			tableLayoutPanel2.Controls.Add(textBox3, 0, 5);
+			tableLayoutPanel2.Controls.Add(labelLink, 0, 4);
+			tableLayoutPanel2.Controls.Add(label2, 0, 2);
+			tableLayoutPanel2.Controls.Add(textBox2, 0, 3);
+			tableLayoutPanel2.Location = new Point(12, 12);
+			tableLayoutPanel2.Name = "tableLayoutPanel2";
+			tableLayoutPanel2.RowCount = 7;
+			tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+			tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 28F));
+			tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+			tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+			tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+			tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+			tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 48F));
+			tableLayoutPanel2.Size = new Size(296, 302);
+			tableLayoutPanel2.TabIndex = 7;
+			// 
+			// FormModifyLesson
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(320, 326);
+			Controls.Add(tableLayoutPanel2);
+			Icon = (Icon)resources.GetObject("$this.Icon");
+			Name = "FormModifyLesson";
+			Text = "Modify Lesson";
+			tableLayoutPanel1.ResumeLayout(false);
+			tableLayoutPanel2.ResumeLayout(false);
+			tableLayoutPanel2.PerformLayout();
+			ResumeLayout(false);
+		}
+
+		#endregion
+
+		private TextBox textBox1;
+		private Label label1;
+		private Label label2;
+		private TextBox textBox2;
+		private TextBox textBox3;
+		private Label labelLink;
+		private TableLayoutPanel tableLayoutPanel1;
+		private Button buttonCancel;
+		private Button buttonApply;
+		private TableLayoutPanel tableLayoutPanel2;
+	}
+}
---
+++ Romplate/MainForm.Designer.cs
@@ -0,0 +1,392 @@
+﻿namespace Romplate
+{
+	partial class MainForm
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			listBoxLessons = new ListBox();
+			numericUpDownWeekDays = new NumericUpDown();
+			labelDayWeek = new Label();
+			textBoxNotation = new TextBox();
+			labelNotation = new Label();
+			checkBox1 = new CheckBox();
+			menuStrip = new MenuStrip();
+			fileToolStripMenuItem = new ToolStripMenuItem();
+			newToolStripMenuItem = new ToolStripMenuItem();
+			weekToolStripMenuItem = new ToolStripMenuItem();
+			templateToolStripMenuItem = new ToolStripMenuItem();
+			saveToolStripMenuItem = new ToolStripMenuItem();
+			openToolStripMenuItem = new ToolStripMenuItem();
+			weekToolStripMenuItem1 = new ToolStripMenuItem();
+			templateToolStripMenuItem1 = new ToolStripMenuItem();
+			exitToolStripMenuItem = new ToolStripMenuItem();
+			aboutToolStripMenuItem = new ToolStripMenuItem();
+			openFileDialog = new OpenFileDialog();
+			saveFileDialog = new SaveFileDialog();
+			tableLayoutPanelInteractors = new TableLayoutPanel();
+			buttonGoToMeet = new Button();
+			buttonDeleteLesson = new Button();
+			buttonModifyLesson = new Button();
+			buttonCreateLesson = new Button();
+			labelHomeworkSign = new Label();
+			textBoxHomework = new TextBox();
+			tableLayoutPanel1 = new TableLayoutPanel();
+			tableLayoutPanel2 = new TableLayoutPanel();
+			tableLayoutPanel3 = new TableLayoutPanel();
+			((System.ComponentModel.ISupportInitialize)numericUpDownWeekDays).BeginInit();
+			menuStrip.SuspendLayout();
+			tableLayoutPanelInteractors.SuspendLayout();
+			tableLayoutPanel1.SuspendLayout();
+			tableLayoutPanel2.SuspendLayout();
+			tableLayoutPanel3.SuspendLayout();
+			SuspendLayout();
+			// 
+			// listBoxLessons
+			// 
+			listBoxLessons.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+			listBoxLessons.FormattingEnabled = true;
+			listBoxLessons.ItemHeight = 15;
+			listBoxLessons.Location = new Point(3, 3);
+			listBoxLessons.Name = "listBoxLessons";
+			listBoxLessons.Size = new Size(190, 319);
+			listBoxLessons.TabIndex = 0;
+			// 
+			// numericUpDownWeekDays
+			// 
+			numericUpDownWeekDays.Location = new Point(3, 357);
+			numericUpDownWeekDays.Maximum = new decimal(new int[] { 7, 0, 0, 0 });
+			numericUpDownWeekDays.Name = "numericUpDownWeekDays";
+			numericUpDownWeekDays.Size = new Size(82, 23);
+			numericUpDownWeekDays.TabIndex = 1;
+			// 
+			// labelDayWeek
+			// 
+			labelDayWeek.AutoSize = true;
+			labelDayWeek.Location = new Point(3, 326);
+			labelDayWeek.Name = "labelDayWeek";
+			labelDayWeek.Size = new Size(69, 15);
+			labelDayWeek.TabIndex = 2;
+			labelDayWeek.Text = "day in week";
+			labelDayWeek.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// textBoxNotation
+			// 
+			textBoxNotation.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			textBoxNotation.Enabled = false;
+			textBoxNotation.Location = new Point(271, 20);
+			textBoxNotation.Multiline = true;
+			textBoxNotation.Name = "textBoxNotation";
+			textBoxNotation.Size = new Size(156, 194);
+			textBoxNotation.TabIndex = 4;
+			// 
+			// labelNotation
+			// 
+			labelNotation.AutoSize = true;
+			labelNotation.Location = new Point(271, 0);
+			labelNotation.Name = "labelNotation";
+			labelNotation.Size = new Size(54, 15);
+			labelNotation.TabIndex = 5;
+			labelNotation.Text = "Notation";
+			// 
+			// checkBox1
+			// 
+			checkBox1.AutoSize = true;
+			checkBox1.Enabled = false;
+			checkBox1.Location = new Point(3, 169);
+			checkBox1.Name = "checkBox1";
+			checkBox1.Size = new Size(54, 19);
+			checkBox1.TabIndex = 6;
+			checkBox1.Text = "Done";
+			checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// menuStrip
+			// 
+			menuStrip.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, aboutToolStripMenuItem });
+			menuStrip.Location = new Point(0, 0);
+			menuStrip.Name = "menuStrip";
+			menuStrip.Size = new Size(655, 24);
+			menuStrip.TabIndex = 7;
+			menuStrip.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { newToolStripMenuItem, saveToolStripMenuItem, openToolStripMenuItem, exitToolStripMenuItem });
+			fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			fileToolStripMenuItem.Size = new Size(37, 20);
+			fileToolStripMenuItem.Text = "File";
+			// 
+			// newToolStripMenuItem
+			// 
+			newToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { weekToolStripMenuItem, templateToolStripMenuItem });
+			newToolStripMenuItem.Name = "newToolStripMenuItem";
+			newToolStripMenuItem.Size = new Size(103, 22);
+			newToolStripMenuItem.Text = "New";
+			// 
+			// weekToolStripMenuItem
+			// 
+			weekToolStripMenuItem.Name = "weekToolStripMenuItem";
+			weekToolStripMenuItem.Size = new Size(122, 22);
+			weekToolStripMenuItem.Text = "Week";
+			// 
+			// templateToolStripMenuItem
+			// 
+			templateToolStripMenuItem.Name = "templateToolStripMenuItem";
+			templateToolStripMenuItem.Size = new Size(122, 22);
+			templateToolStripMenuItem.Text = "Template";
+			// 
+			// saveToolStripMenuItem
+			// 
+			saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+			saveToolStripMenuItem.Size = new Size(103, 22);
+			saveToolStripMenuItem.Text = "Open";
+			// 
+			// openToolStripMenuItem
+			// 
+			openToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { weekToolStripMenuItem1, templateToolStripMenuItem1 });
+			openToolStripMenuItem.Name = "openToolStripMenuItem";
+			openToolStripMenuItem.Size = new Size(103, 22);
+			openToolStripMenuItem.Text = "Save";
+			// 
+			// weekToolStripMenuItem1
+			// 
+			weekToolStripMenuItem1.Name = "weekToolStripMenuItem1";
+			weekToolStripMenuItem1.Size = new Size(122, 22);
+			weekToolStripMenuItem1.Text = "Week";
+			// 
+			// templateToolStripMenuItem1
+			// 
+			templateToolStripMenuItem1.Name = "templateToolStripMenuItem1";
+			templateToolStripMenuItem1.Size = new Size(122, 22);
+			templateToolStripMenuItem1.Text = "Template";
+			// 
+			// exitToolStripMenuItem
+			// 
+			exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			exitToolStripMenuItem.Size = new Size(103, 22);
+			exitToolStripMenuItem.Text = "Exit";
+			// 
+			// aboutToolStripMenuItem
+			// 
+			aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+			aboutToolStripMenuItem.Size = new Size(52, 20);
+			aboutToolStripMenuItem.Text = "About";
+			// 
+			// openFileDialog
+			// 
+			openFileDialog.FileName = "openFileDialog1";
+			// 
+			// tableLayoutPanelInteractors
+			// 
+			tableLayoutPanelInteractors.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			tableLayoutPanelInteractors.ColumnCount = 1;
+			tableLayoutPanelInteractors.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+			tableLayoutPanelInteractors.Controls.Add(buttonGoToMeet, 0, 3);
+			tableLayoutPanelInteractors.Controls.Add(buttonDeleteLesson, 0, 2);
+			tableLayoutPanelInteractors.Controls.Add(buttonModifyLesson, 0, 1);
+			tableLayoutPanelInteractors.Controls.Add(buttonCreateLesson, 0, 0);
+			tableLayoutPanelInteractors.Location = new Point(210, 263);
+			tableLayoutPanelInteractors.Name = "tableLayoutPanelInteractors";
+			tableLayoutPanelInteractors.RowCount = 4;
+			tableLayoutPanelInteractors.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+			tableLayoutPanelInteractors.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+			tableLayoutPanelInteractors.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+			tableLayoutPanelInteractors.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+			tableLayoutPanelInteractors.Size = new Size(433, 156);
+			tableLayoutPanelInteractors.TabIndex = 8;
+			// 
+			// buttonGoToMeet
+			// 
+			buttonGoToMeet.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			buttonGoToMeet.Location = new Point(3, 120);
+			buttonGoToMeet.Name = "buttonGoToMeet";
+			buttonGoToMeet.Size = new Size(427, 33);
+			buttonGoToMeet.TabIndex = 5;
+			buttonGoToMeet.Text = "Go to meet";
+			buttonGoToMeet.UseVisualStyleBackColor = true;
+			// 
+			// buttonDeleteLesson
+			// 
+			buttonDeleteLesson.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			buttonDeleteLesson.Location = new Point(3, 81);
+			buttonDeleteLesson.Name = "buttonDeleteLesson";
+			buttonDeleteLesson.Size = new Size(427, 33);
+			buttonDeleteLesson.TabIndex = 4;
+			buttonDeleteLesson.Text = "Delete Lesson";
+			buttonDeleteLesson.UseVisualStyleBackColor = true;
+			// 
+			// buttonModifyLesson
+			// 
+			buttonModifyLesson.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			buttonModifyLesson.Location = new Point(3, 42);
+			buttonModifyLesson.Name = "buttonModifyLesson";
+			buttonModifyLesson.Size = new Size(427, 33);
+			buttonModifyLesson.TabIndex = 2;
+			buttonModifyLesson.Text = "Modify Lesson";
+			buttonModifyLesson.UseVisualStyleBackColor = true;
+			// 
+			// buttonCreateLesson
+			// 
+			buttonCreateLesson.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			buttonCreateLesson.Location = new Point(3, 3);
+			buttonCreateLesson.Name = "buttonCreateLesson";
+			buttonCreateLesson.Size = new Size(427, 33);
+			buttonCreateLesson.TabIndex = 0;
+			buttonCreateLesson.Text = "Create Lesson";
+			buttonCreateLesson.UseVisualStyleBackColor = true;
+			// 
+			// labelHomeworkSign
+			// 
+			labelHomeworkSign.AutoSize = true;
+			labelHomeworkSign.Location = new Point(3, 0);
+			labelHomeworkSign.Name = "labelHomeworkSign";
+			labelHomeworkSign.Size = new Size(66, 15);
+			labelHomeworkSign.TabIndex = 9;
+			labelHomeworkSign.Text = "Homework";
+			// 
+			// textBoxHomework
+			// 
+			textBoxHomework.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			textBoxHomework.Enabled = false;
+			textBoxHomework.Location = new Point(3, 3);
+			textBoxHomework.Multiline = true;
+			textBoxHomework.Name = "textBoxHomework";
+			textBoxHomework.ReadOnly = true;
+			textBoxHomework.Size = new Size(256, 160);
+			textBoxHomework.TabIndex = 10;
+			// 
+			// tableLayoutPanel1
+			// 
+			tableLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+			tableLayoutPanel1.ColumnCount = 1;
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+			tableLayoutPanel1.Controls.Add(numericUpDownWeekDays, 0, 2);
+			tableLayoutPanel1.Controls.Add(listBoxLessons, 0, 0);
+			tableLayoutPanel1.Controls.Add(labelDayWeek, 0, 1);
+			tableLayoutPanel1.Location = new Point(4, 37);
+			tableLayoutPanel1.Name = "tableLayoutPanel1";
+			tableLayoutPanel1.RowCount = 3;
+			tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+			tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 28F));
+			tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 28F));
+			tableLayoutPanel1.Size = new Size(200, 382);
+			tableLayoutPanel1.TabIndex = 11;
+			// 
+			// tableLayoutPanel2
+			// 
+			tableLayoutPanel2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			tableLayoutPanel2.ColumnCount = 1;
+			tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+			tableLayoutPanel2.Controls.Add(checkBox1, 0, 1);
+			tableLayoutPanel2.Controls.Add(textBoxHomework, 0, 0);
+			tableLayoutPanel2.Location = new Point(3, 20);
+			tableLayoutPanel2.Name = "tableLayoutPanel2";
+			tableLayoutPanel2.RowCount = 2;
+			tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+			tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 28F));
+			tableLayoutPanel2.Size = new Size(262, 194);
+			tableLayoutPanel2.TabIndex = 12;
+			// 
+			// tableLayoutPanel3
+			// 
+			tableLayoutPanel3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			tableLayoutPanel3.ColumnCount = 2;
+			tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 62.5F));
+			tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 37.5F));
+			tableLayoutPanel3.Controls.Add(labelHomeworkSign, 0, 0);
+			tableLayoutPanel3.Controls.Add(tableLayoutPanel2, 0, 1);
+			tableLayoutPanel3.Controls.Add(labelNotation, 1, 0);
+			tableLayoutPanel3.Controls.Add(textBoxNotation, 1, 1);
+			tableLayoutPanel3.Location = new Point(210, 40);
+			tableLayoutPanel3.Name = "tableLayoutPanel3";
+			tableLayoutPanel3.RowCount = 2;
+			tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Absolute, 17F));
+			tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+			tableLayoutPanel3.Size = new Size(430, 217);
+			tableLayoutPanel3.TabIndex = 13;
+			// 
+			// MainForm
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(655, 431);
+			Controls.Add(tableLayoutPanel3);
+			Controls.Add(tableLayoutPanel1);
+			Controls.Add(tableLayoutPanelInteractors);
+			Controls.Add(menuStrip);
+			Icon = (Icon)resources.GetObject("$this.Icon");
+			MainMenuStrip = menuStrip;
+			MinimumSize = new Size(405, 396);
+			Name = "MainForm";
+			Text = "Romplate";
+			((System.ComponentModel.ISupportInitialize)numericUpDownWeekDays).EndInit();
+			menuStrip.ResumeLayout(false);
+			menuStrip.PerformLayout();
+			tableLayoutPanelInteractors.ResumeLayout(false);
+			tableLayoutPanel1.ResumeLayout(false);
+			tableLayoutPanel1.PerformLayout();
+			tableLayoutPanel2.ResumeLayout(false);
+			tableLayoutPanel2.PerformLayout();
+			tableLayoutPanel3.ResumeLayout(false);
+			tableLayoutPanel3.PerformLayout();
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private ListBox listBoxLessons;
+		private NumericUpDown numericUpDownWeekDays;
+		private Label labelDayWeek;
+		private TextBox textBoxNotation;
+		private Label labelNotation;
+		private CheckBox checkBox1;
+		private MenuStrip menuStrip;
+		private ToolStripMenuItem fileToolStripMenuItem;
+		private ToolStripMenuItem newToolStripMenuItem;
+		private ToolStripMenuItem saveToolStripMenuItem;
+		private ToolStripMenuItem openToolStripMenuItem;
+		private ToolStripMenuItem exitToolStripMenuItem;
+		private ToolStripMenuItem aboutToolStripMenuItem;
+		private ToolStripMenuItem templateToolStripMenuItem;
+		private ToolStripMenuItem weekToolStripMenuItem;
+		private ToolStripMenuItem weekToolStripMenuItem1;
+		private ToolStripMenuItem templateToolStripMenuItem1;
+		private OpenFileDialog openFileDialog;
+		private SaveFileDialog saveFileDialog;
+		private TableLayoutPanel tableLayoutPanelInteractors;
+		private Button buttonDeleteLesson;
+		private Button buttonModifyLesson;
+		private Button buttonCreateLesson;
+		private Button buttonGoToMeet;
+		private Label labelHomeworkSign;
+		private TextBox textBoxHomework;
+		private TableLayoutPanel tableLayoutPanel1;
+		private TableLayoutPanel tableLayoutPanel2;
+		private TableLayoutPanel tableLayoutPanel3;
+	}
+}
